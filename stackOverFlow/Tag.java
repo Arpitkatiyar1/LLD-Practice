a015ee9b8afbced005f4adc96b31a4e5ce3813d2
arpitKatiyar <72797299+Arpitkatiyar1@users.noreply.github.com>
2025-04-12 14:37:28 +0530

    Stack of 01 (#6)
    * feat(parkinglot) #WBR-01 have designed a parkinglot
    
    * feat(parkinglot) #WBR-01 have designed a parkinglot
    
    * feat(stackoverflow) #StackOF-01 created template for stackoverflow
    
    * feat(parkinglot) #StackOF-01 created tag class
    
    * feat(parkinglot) #StackOF-01 created tag class

--- stackOverFlow/Tag.java
+++ stackOverFlow/Tag.java
@@ -1,5 +1,14 @@
 package stackOverFlow;
 
 public class Tag {
-    
+    private int id;
+    private String name;
+
+    Tag(int id, String name){
+        this.id=id;
+        this.name=name;
+    }
+    public synchronized int getId(){
+        return this.id;
+    }
 }
