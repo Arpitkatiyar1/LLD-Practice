16d46c0840ce0e55f64ce6928be20c595efb5795
Arpit Katiyar <ktrarpit@gmail.com>
2025-04-12 14:16:51 +0530

    feat(parkinglot) #StackOF-01 created tag class

--- stackOverFlow/Tag.java
+++ stackOverFlow/Tag.java
@@ -1,5 +1,17 @@
 package stackOverFlow;
 
 public class Tag {
-    
+    private int id;
+    private String name;
+
+    Tag(int id, String name){
+        this.id=id;
+        this.name=name;
+    }
+    public synchronized int getId(){
+        return this.id;
+    }
+    public synchronized String getName(){
+        return this.name;
+    }
 }
