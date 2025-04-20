70d920d781de8202070468f8c879958b38175299
arpitKatiyar <72797299+Arpitkatiyar1@users.noreply.github.com>
2025-04-20 22:12:43 +0530

    fix: deleted nested lld directory (#19)

--- app/src/main/java/lld_practice/stackOverFlow/StackOverFlowDemo.java
+++
@@ -1,7 +0,0 @@
-package app.src.main.java.lld_practice.stackOverFlow;
-
-public class StackOverFlowDemo {
-    public static void main(String[] args){
-     
-    }
-}
--- app/src/main/java/lld_practice/stackOverFlow/interfaces/Votable.java
+++
@@ -1,8 +0,0 @@
-package app.src.main.java.lld_practice.stackOverFlow.interfaces;
-
-import app.src.main.java.lld_practice.stackOverFlow.Vote;
-
-public interface Votable {
-    void updateVote(Vote vote);
-    int getVotes();
-}
